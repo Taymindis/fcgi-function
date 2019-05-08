57f2798058d00f89a6850d396f845ed1399b5ac7
Woon Sze Hsiang <woonsh@comfortdelgro.com>
2019-05-08 16:52:41 +0800

    modified start script for example

--- DockerExample/start_ffunc.sh
+++ DockerExample/start_ffunc.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sed -i '/include conf.d\/ffunc_profile;/d  ' /etc/nginx/conf.d/default.conf
 sed -i '/listen[[:space:]]\+80;/a include conf.d/ffunc_profile;' /etc/nginx/conf.d/default.conf
 
 gcc -I/tmp/fcgi-function /tmp/profile_service_daemonize.c /tmp/fcgi-function/ffunc.c -lfcgi -pthread -ldl -rdynamic -o ffuncsample
